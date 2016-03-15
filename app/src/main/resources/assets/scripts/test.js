a601e8ad861a9a1c02ebfe23fba9bf9a2ced6b4c
blseich <blseich@gmail.com>
2016-03-15 16:59:06 -0400

    Add button with toggle js function. Lay out web-app

--- app/src/main/resources/assets/scripts/test.js
+++ app/src/main/resources/assets/scripts/test.js
@@ -1,1 +1,29 @@
 console.log("HOLY MOLY MACARONI THIS WORKS TOO!!");
+
+
+Timer = (function(){
+	
+	var started = false;
+	
+	function _stopTimer() {
+		console.log("TIMER WAS STOPPED!");
+		started = !started;
+	}
+	
+	function _startTimer() {
+		console.log("TIMER WAS STARTED");
+		started = !started;
+	}
+	
+	function toggleTimer() {
+		if (started) {
+			_stopTimer();
+		} else {
+			_startTimer();
+		}	
+	}
+	
+	return {
+		toggleTimer: toggleTimer
+	};
+})();
