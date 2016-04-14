1a351faf6b31a25be6376803118b64a03e3f759b
Alexa McIlrath <rachel_mcilrath@gap.com>
2016-04-14 10:51:32 -0400

    refactoring toggleTimer to reduce complexity

--- app/src/main/resources/assets/scripts/test.js
+++ app/src/main/resources/assets/scripts/test.js
@@ -1,51 +1,20 @@
-console.log("HOLY MOLY MACARONI THIS WORKS TOO!!");
-
-
 Timer = (function(){
 	
 	var started = false;
 	
-	function _stopTimer() {
-		console.log("TIMER WAS STOPPED!");
-		started = false;
-		
-		$.ajax({
-			url: "/calculate/costByDuration/83",
-			type: "GET",
-			contentType: 'application/json; charset=utf-8',
-            success: function(resultData) {
-                console.log(resultData);
-
-            },
-            error : function(jqXHR, textStatus, errorThrown) {
-            },
-
-            timeout: 120000
-		});
-	}
-	
-	function _startTimer() {
-		console.log("TIMER WAS STARTED");
-		started = true;
-	}
-	
 	function toggleTimer() {
-		if (started) {
-			_stopTimer();
-			
-		} else {
-			_startTimer();
-		}	
+		started = !started	
+		console.log("Timer status: " + started);
 	}
 	
-	function timerState() {
+	function isStarted() {
 		return started;
 	}
 	
-	
 	return {
 		toggleTimer: toggleTimer,
-		started: timerState
+		isStarted: isStarted
 	};
 })();
 module.exports = Timer;
+
--- app/src/test/javascript/spec/TimerSpec.js
+++ app/src/test/javascript/spec/TimerSpec.js
@@ -1,10 +1,7 @@
 var timer = require('../../../../src/main/resources/assets/scripts/test.js')
 describe( "timer", function () {
     it("starts the timer", function () {
-    	console.log("before toggle: timer.started=" + timer.started());
     	timer.toggleTimer();
-    	console.log("after toggle: timer.started=" + timer.started());
-    	var started = timer.started();
-        expect(started).toEqual(true);
+        expect(timer.isStarted()).toEqual(true);
     });
 });
