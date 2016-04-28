60a833eac3e146ea4f6018b89e11e687635da8d4
blseich <blseich@gmail.com>
2016-04-28 11:01:41 -0400

    incomplete moment test

--- app/src/test/javascript/spec/timer-spec.js
+++ app/src/test/javascript/spec/timer-spec.js
@@ -3,21 +3,24 @@ $ = {
 	'ajax': function() {}
 };
 
+moment = function() {}
+
 describe("timer", function () {
 	var timerUnderTest;
 	beforeEach(function() {
 		timerUnderTest = new timer();
 		spyOn($, 'ajax');
+		spyOn(global, 'moment');
 	});
 	
     it("starts the timer", function () {
-    	timerUnderTest.toggleTimer();
+    		timerUnderTest.toggleTimer();
         expect(timerUnderTest.isStarted()).toEqual(true);
     });
     
     it("stops the timer", function () {
-    	timerUnderTest.toggleTimer();
-    	timerUnderTest.toggleTimer();
+    		timerUnderTest.toggleTimer();
+    		timerUnderTest.toggleTimer();
         expect(timerUnderTest.isStarted()).toEqual(false);
     });
     
@@ -28,13 +31,18 @@ describe("timer", function () {
     });
     
     it("should not send timer data on timer start", function() {
-    	timerUnderTest.toggleTimer();
-    	expect($.ajax).not.toHaveBeenCalled();
+    		timerUnderTest.toggleTimer();
+    		expect($.ajax).not.toHaveBeenCalled();
     });
     
     it("should call costByDuration url", function() {
-    	timerUnderTest.toggleTimer();
-    	timerUnderTest.toggleTimer();
-    	expect($.ajax).toHaveBeenCalledWith(jasmine.objectContaining({url:"calculate/costByDuration/100"}))
+    		timerUnderTest.toggleTimer();
+    		timerUnderTest.toggleTimer();
+    		expect($.ajax).toHaveBeenCalledWith(jasmine.objectContaining({url:"calculate/costByDuration/100"}))
+    });
+    
+    it("should call moment on first button press", function() {
+    		timerUnderTest.toggleTimer();
+    		expect(global.moment).toHaveBeenCalled();
     });
 });
