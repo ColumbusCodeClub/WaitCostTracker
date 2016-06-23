7ce99bc04908bb2eb8e2aca83eace5cac4e8283e
Alexa McIlrath <rachel_mcilrath@gap.com>
2016-06-23 12:01:47 -0400

    makin the javascript pretty/modular

--- src/main/resources/assets/scripts/write-to-page.js
+++ src/main/resources/assets/scripts/write-to-page.js
@@ -1,25 +1,18 @@
 PageWriter = function(){
 	function writeCost(value) {
-		console.log("cost value: " + value);
-		$('#cost').html(value);
+		$('#cost').html("$" + value);
 	}
 	
 	function writeTime(value) {
-		console.log("time value: " + value);
-		$('#time').html(value);
+		$('#time').html(value + " minutes");
 	}
 	
-	function writeTimeCostJson(value) {
+	this.writeTimeCostJson = function(value) {
 		console.log("json value: " + value);
 		var obj = JSON.parse(value);
 		writeCost(obj.cost.value);
 		writeTime(obj.duration);
 	}
-	
-	return {
-		writeCost: writeCost,
-		writeTime: writeTime,
-		writeTimeCostJson: writeTimeCostJson
-	};
+
 }
 module.exports = PageWriter;
--- src/test/javascript/spec/write-to-page-spec.js
+++ src/test/javascript/spec/write-to-page-spec.js
@@ -20,24 +20,9 @@ describe("writer", function () {
 		spyOn(jQueryMockObject, 'html');		
 	});
 	
-	it("writes hello world to the page", function() {
-		pageWriterUnderTest.writeCost("hello world");
-		expect(jQueryMockObject.html).toHaveBeenCalledWith('hello world');
-	});
-
-	it("writes cost to the page", function() {
-		pageWriterUnderTest.writeCost('at 100.00 per hr');
-		expect(jQueryMockObject.html).toHaveBeenCalledWith('at 100.00 per hr');
-	});
-	
-	it("writes time to the page", function() {
-		pageWriterUnderTest.writeTime('1 hour');
-		expect(jQueryMockObject.html).toHaveBeenCalledWith('1 hour');
-	});
-	
 	it("writes time and cost to the page", function() {
 		pageWriterUnderTest.writeTimeCostJson('{"duration":"00:05", "cost":{"value":"12.00"}}');
-		expect(jQueryMockObject.html).toHaveBeenCalledWith("00:05");
-		expect(jQueryMockObject.html).toHaveBeenCalledWith("12.00");
+		expect(jQueryMockObject.html).toHaveBeenCalledWith("00:05 minutes");
+		expect(jQueryMockObject.html).toHaveBeenCalledWith("$12.00");
 	})
 });
