4bb68c85a74bd933361bdccf854b59a523ede255
Nate <nathan_halterman@gap.com>
2016-07-14 11:52:27 -0400

    implement cookie handler in order to mock request

---
+++ src/main/groovy/org/orsh/waitCostTracker/CookieHandler.groovy
@@ -0,0 +1,15 @@
+package org.orsh.waitCostTracker
+import ratpack.handling.Handler
+import ratpack.handling.Context
+
+public class CookieHandler implements Handler {
+		public void handle(Context ctx) throws Exception {
+		  String outputHeaderValue = ctx.getRequest().getHeaders().get("set-cookie")
+		  if (outputHeaderValue == null) {
+			  outputHeaderValue = "foo=bar"
+		  }
+		  ctx.getResponse().getHeaders().set("set-cookie", outputHeaderValue)
+		  ctx.render("received:")
+		}
+}
+
---
+++ src/test/groovy/org/orsh/waitCostTracker/CookiesHandlerSpec.groovy
@@ -0,0 +1,35 @@
+package org.orsh.waitCostTracker
+
+import static ratpack.jackson.Jackson.json
+import static org.orsh.waitCostTracker.Rate.DEFAULT_RATE
+
+import groovy.json.JsonSlurper
+import ratpack.groovy.test.GroovyRatpackMainApplicationUnderTest
+import ratpack.test.ServerBackedApplicationUnderTest
+import ratpack.test.http.TestHttpClient
+import spock.lang.Specification
+
+import ratpack.test.handling.RequestFixture;
+import ratpack.test.handling.HandlingResult;
+import static org.junit.Assert.assertEquals;
+
+class CookiesHandlerSpec extends Specification {
+
+	def "should not overwrite cookie value if it exists"() {
+		when:
+		HandlingResult result = RequestFixture.handle(new CookieHandler(), {fixture ->
+			fixture.header("set-cookie", "foo=chocolate")}
+		)
+
+		then:
+		assertEquals("foo=chocolate", result.getHeaders().get("set-cookie"))
+	}
+	
+	def "should set cookie value if it does not exist"() {
+		when:
+		HandlingResult result = RequestFixture.handle(new CookieHandler(), {})
+
+		then:
+		assertEquals("foo=bar", result.getHeaders().get("set-cookie"))
+	}
+}
